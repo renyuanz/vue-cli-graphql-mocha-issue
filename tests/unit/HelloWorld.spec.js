5823ad45f80f7a9e9360614940c3018e601da913
Renyuanz <renyuan.zou@s4m.io>
2018-06-20 12:34:03 +0200

    add graphql and update unit test

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,9 +1,12 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
+import { graphql } from 'graphql'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
+    const t = graphql();
+    console.log(t);
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
